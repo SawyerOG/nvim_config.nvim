853e80aa9deb43a5dcba8ad45f4d9eb946041fb6
SawyerOG <oglesbysaw@gmail.com>
2025-03-06 20:37:15 -0700

    completed debug setup

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,41 +1,41 @@
 return {
   {
-    'nvim-neotest/neotest',
+    "nvim-neotest/neotest",
     dependencies = {
-      'nvim-neotest/nvim-nio',
-      'nvim-lua/plenary.nvim',
-      'antoinemadec/FixCursorHold.nvim',
-      'nvim-treesitter/nvim-treesitter',
-      { 'fredrikaverpil/neotest-golang', version = '*' }, -- Installation
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      { "fredrikaverpil/neotest-golang", version = "*" }, -- Installation
     },
     config = function()
       local neotest_golang_opts = {
-        go_test_args = { '-v' }, -- Verbose output like go test -v
+        go_test_args = { "-v" }, -- Verbose output like go test -v
         warn_test_name_dupes = true, -- Warn about duplicate test names
       } -- Specify custom configuration
-      require('neotest').setup {
+      require("neotest").setup {
         adapters = {
-          require 'neotest-golang'(neotest_golang_opts), -- Registration
+          require "neotest-golang"(neotest_golang_opts), -- Registration
         },
       }
 
-      vim.keymap.set('n', '<leader>gt', function()
-        local neotest = require 'neotest'
+      vim.keymap.set("n", "<leader>tf", function()
+        local neotest = require "neotest"
         neotest.run.run()
         neotest.output.open { enter = true }
-      end, { desc = 'Run nearest Go test and show output' })
+      end, { desc = "Run nearest Go test and show output" })
 
-      vim.keymap.set('n', '<leader>go', function()
-        require('neotest').output.open { enter = true }
-      end, { desc = 'Open test output' })
+      vim.keymap.set("n", "<leader>to", function()
+        require("neotest").output.open { enter = true }
+      end, { desc = "Open test output" })
 
-      vim.keymap.set('n', '<leader>gs', function()
-        require('neotest').summary.open()
-      end, { desc = 'Open test summary panel' })
+      vim.keymap.set("n", "<leader>ts", function()
+        require("neotest").summary.open()
+      end, { desc = "Open test summary panel" })
 
-      vim.keymap.set('n', '<leader>gd', function()
-        require('neotest').run.run { vim.fn.expand '%', strategy = 'dap' }
-      end, { desc = 'Run and debug nearest Go test and show output' })
+      vim.keymap.set("n", "<leader>td", function()
+        require("neotest").run.run { strategy = "dap" }
+      end, { desc = "Run and debug nearest Go test and show output" })
     end,
   },
 }
