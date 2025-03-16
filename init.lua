38aa47a9109604c458db6dc62ec8040755538d28
SawyerOG <oglesbysw@gmail.com>
2025-03-16 16:42:17 -0600

    add autotag

--- init.lua
+++ init.lua
@@ -1,38 +1,38 @@
-require 'opts'
-require 'keymaps'
-require 'functions.floaterm'
+require "opts"
+require "keymaps"
+require "functions.floaterm"
 
 vim.opt.guicursor =
-  'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
+  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 --  See `:help lua-guide-autocommands`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
+    error("Error cloning lazy.nvim:\n" .. out)
   end
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+require("lazy").setup({
+  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
   {
-    'ellisonleao/gruvbox.nvim',
+    "ellisonleao/gruvbox.nvim",
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       -- require('tokyonight').setup {
-      require('gruvbox').setup {
+      require("gruvbox").setup {
         terminal_colors = true, -- add neovim terminal colors
         undercurl = true,
         underline = true,
@@ -50,52 +50,53 @@ require('lazy').setup({
         invert_tabline = false,
         invert_intend_guides = false,
         inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = 'hard', -- can be "hard", "soft" or empty string
+        contrast = "hard", -- can be "hard", "soft" or empty string
         palette_overrides = {},
         overrides = {},
         dim_inactive = false,
         transparent_mode = false,
       }
 
-      vim.cmd.colorscheme 'gruvbox'
+      vim.cmd.colorscheme "gruvbox"
     end,
   },
 
-  require 'plugins.debug',
-  require 'plugins.indent_line',
-  require 'plugins.lint',
-  require 'plugins.autopairs',
-  require 'plugins.neo-tree',
-  require 'plugins.gitsigns',
-  require 'plugins.whichkey',
-  require 'plugins.telescope',
-  require 'plugins.lsp',
-  require 'plugins.treesitter',
-  require 'plugins.autocompletion',
-  require 'plugins.autoformat',
-  require 'plugins.mini',
-  require 'plugins.neotest',
-  require 'plugins.bufferline',
-  require 'plugins.rainbow-delim',
-  require 'plugins.flash',
+  require "plugins.debug",
+  require "plugins.indent_line",
+  require "plugins.lint",
+  require "plugins.autopairs",
+  require "plugins.neo-tree",
+  require "plugins.gitsigns",
+  require "plugins.whichkey",
+  require "plugins.telescope",
+  require "plugins.lsp",
+  require "plugins.treesitter",
+  require "plugins.autocompletion",
+  require "plugins.autoformat",
+  require "plugins.mini",
+  require "plugins.neotest",
+  require "plugins.bufferline",
+  require "plugins.rainbow-delim",
+  require "plugins.flash",
+  require "plugins.autotag",
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
     },
   },
 })
