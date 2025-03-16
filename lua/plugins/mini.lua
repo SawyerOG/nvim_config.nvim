767d5f3cb36f0bcfba032d3128c3090880333a07
SawyerOG <oglesbysw@gmail.com>
2025-03-16 09:49:08 -0600

    twaeks

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'echasnovski/mini.nvim',
+    "echasnovski/mini.nvim",
     config = function()
       -- Better Around/Inside textobjects
       --
@@ -8,37 +8,39 @@ return {
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
+      require("mini.ai").setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
 
-      require('mini.comment').setup {
+      vim.keymap.set({ "n", "x" }, "s", "<Nop>")
+      require("mini.surround").setup()
+
+      require("mini.comment").setup {
         mappings = {
           -- Toggle comment (like `gcip` - comment inner paragraph) for both
           -- Normal and Visual modes
-          comment = '<leader>c',
+          comment = "<leader>c",
 
           -- Toggle comment on current line
-          comment_line = '<leader>c',
+          comment_line = "<leader>c",
 
           -- Toggle comment on visual selection
-          comment_visual = '<leader>c',
+          comment_visual = "<leader>c",
 
           -- Define 'comment' textobject (like `dgc` - delete whole comment block)
           -- Works also in Visual mode if mapping differs from `comment_visual`
-          textobject = '<leader>c',
+          textobject = "<leader>c",
         },
       }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
+      local statusline = require "mini.statusline"
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
@@ -47,7 +49,7 @@ return {
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return "%2l:%-2v"
       end
 
       -- ... and there is more!
