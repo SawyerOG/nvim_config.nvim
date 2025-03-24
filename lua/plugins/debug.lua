2bb5c3d1e134128cb1ea53b25ab646ac93b3de94
SawyerOG <oglesbysaw@gmail.com>
2025-03-24 12:41:33 -0600

    trying to make things more lazy

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -9,6 +9,8 @@
 return {
   -- NOTE: Yes, you can install new plugins here!
   "mfussenegger/nvim-dap",
+  lazy = true,
+  event = "VeryLazy",
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
